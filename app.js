e251c467fa6513c7126284600ede7f364173cc86
hamdidrsy <76738353+hamdidrsy@users.noreply.github.com>
2026-08-25 14:04:03 +0300

    z

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 const baslatButonu = document.querySelector("#mavi");
-const başlangıc = document.querySelector("#başlangıc")
-const soruKutusu = document.querySelector("#soruKutusu")
+
+const başlangıc = document.querySelector("#başlangıc");
+
+const soruKutusu = document.querySelector("#soruKutusu");
+
+const tablo = document.querySelector("#tablo");
+
+
+let dogruY = 0;
+
+let yanlisY = 0;
+
+let puanY = 0;
+
 
 baslatButonu.addEventListener("click", function () {
 
     başlangıc.style.display = "none";
 
     soruKutusu.style.display = "block";
+
 });
 
 
-let dogruY = 0;
-let yanlısY = 0;
-let puanY = 0;
+function cevap(dogruMu) {
 
+    if (dogruMu) {
 
-function cevap(dogruMu){
-    if(dogruMu){
         dogruY = dogruY + 1;
+
         puanY = puanY + 10;
-    }else{
-        yanlısY = yanlısY +1 ;
+
+    } else {
+
+        yanlisY = yanlisY + 1;
+
     }
 
+
     dogru.textContent = dogruY;
-    yanlıs.textContent = yanlısY;
-    puanY.textContent = puanY;
+
+    yanlis.textContent = yanlisY;
+
+    puan.textContent = puanY;
+
 
     soruKutusu.style.display = "none";
+
     tablo.style.display = "block";
+
 }
