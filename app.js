eb484d549f440adf8fc7f1b075b1c7c92cf5666f
hamdidrsy <76738353+hamdidrsy@users.noreply.github.com>
2026-08-25 15:55:01 +0300

    15. ders soru ekleme

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const başlangıc = document.querySelector("#başlangıc");
 
 const soruKutusu = document.querySelector("#soruKutusu");
 
+const soruKutusu2 = document.querySelector("#soruKutusu2")
+
+const soruKutusu3 = document.querySelector("#soruKutusu3")
+
+const soruKutusu4 = document.querySelector("#soruKutusu4")
+
+
 const tablo = document.querySelector("#tablo");
 
 
@@ -37,16 +44,62 @@ function cevap(dogruMu) {
 
     }
 
-
     dogru.textContent = dogruY;
-
-    yanlis.textContent = yanlisY;
-
+    yanlış.textContent = yanlisY;
     puan.textContent = puanY;
 
-
     soruKutusu.style.display = "none";
+    soruKutusu2.style.display = "block";
 
+}
+
+function cevap2(dogruMu) {
+
+    if (dogruMu) {
+        dogruY = dogruY + 1;
+        puanY = puanY + 10;
+    } else {
+        yanlisY = yanlisY + 1;
+    }
+
+    dogru.textContent = dogruY;
+    yanlış.textContent = yanlisY;
+    puan.textContent = puanY;
+
+    soruKutusu2.style.display = "none";
+    soruKutusu3.style.display = "block";
+}
+
+function cevap3(dogruMu) {
+
+    if (dogruMu) {
+        dogruY = dogruY + 1;
+        puanY = puanY + 10;
+    } else {
+        yanlisY = yanlisY + 1;
+    }
+
+    dogru.textContent = dogruY;
+    yanlış.textContent = yanlisY;
+    puan.textContent = puanY;
+
+    soruKutusu3.style.display = "none";
+    soruKutusu4.style.display = "block";
+}
+
+function cevap4(dogruMu) {
+
+    if (dogruMu) {
+        dogruY = dogruY + 1;
+        puanY = puanY + 10;
+    } else {
+        yanlisY = yanlisY + 1;
+    }
+
+    dogru.textContent = dogruY;
+    yanlış.textContent = yanlisY;
+    puan.textContent = puanY;
+
+    soruKutusu4.style.display = "none";
     tablo.style.display = "block";
-
 }
